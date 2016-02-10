8ec15f7d285e4493e020f991e62a897ce6cbb12a
sntwo <joshuaknapp@me.com>
2016-02-09 17:38:09 -0800

    Initial Commit

---
+++ S2Spectra/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  S2Spectra
+//
+//  Created by Joshua Knapp on 2/9/16.
+//  Copyright © 2016 SnTwo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
